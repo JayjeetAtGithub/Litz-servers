7a6cbe4d77093e8197a32aa0a93c86a15d4e6ba6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-02-17 12:29:59 -0800

    Add script to check occupancy of maxwell

--- check_sizes.py
+++ check_sizes.py
@@ -1,6 +1,18 @@
 import os    
 import sys
 
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    return total_size
+
+
 if __name__ == "__main__":
     target_dir = str(sys.argv[1])
     list_of_directories = os.listdir(target_dir)
@@ -8,7 +20,7 @@ if __name__ == "__main__":
         path = os.path.join(target_dir, directory)
         if os.path.isdir(path):
             # get the size of the directory
-            size = os.path.getsize(path)
+            size = get_size(path)
             print(f"Directory: {path} - Size: {size} bytes")
         else:
             # get the size of the file
